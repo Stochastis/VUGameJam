9f7f9525e26f51d18a5725e4de61a0465f0189fb
Caleb Ross <stochastis3@gmail.com>
2025-07-02 14:36:49 -0500

    Globally interactive doors
    Need to fix tilemaplayers closed door sprites overlaying on top of open doors

---
+++ door.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+# Not sure if these need to be exported or not
+@export var open: bool = true
+@export var northSouth: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if open:
+		openDoor()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if open:
+			closeDoor()
+			return
+		else:
+			print("OpeningDoor")
+			openDoor()
+
+func openDoor() -> void:
+	open = true
+	if northSouth:
+		$AnimatedSprite2D.set_frame_and_progress(1, 0)
+	else:
+		$AnimatedSprite2D.set_frame_and_progress(2, 0)
+	$StaticBody2D/CollisionShape2D2.disabled = true
+
+func closeDoor() -> void:
+	open = false
+	$AnimatedSprite2D.set_frame_and_progress(0, 0)
+	$StaticBody2D/CollisionShape2D2.disabled = false
---
+++ main.gd
@@ -0,0 +1,37 @@
+extends Node
+
+# world.gd or whatever your main level scene is
+@onready var tileMapLayer: TileMapLayer = $"TileMapLayers/TileMapLayer-Walls"
+@export var door_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_doors()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_doors():
+	for cell_pos in tileMapLayer.get_used_cells():
+			var tile_data = tileMapLayer.get_cell_tile_data(cell_pos)
+			if tile_data == null:
+				continue
+
+			# Only process tiles marked as door
+			if not tile_data.get_custom_data("is_door"):
+				continue
+
+			var door = door_scene.instantiate()
+
+			# Convert cell coordinates to world position
+			var local_pos = tileMapLayer.map_to_local(cell_pos)
+			door.global_position = tileMapLayer.to_global(local_pos)
+
+			# Read and apply custom data
+			if tile_data.has_custom_data("north_south"):
+				door.northSouth = tile_data.get_custom_data("north_south")
+			if tile_data.has_custom_data("open"):
+				door.open = tile_data.get_custom_data("open")
+
+			add_child(door)
