33f423e57e13d960684df8487a35c227d7d414ef
typhus733 <typhus733@gmail.com>
2025-07-07 23:54:24 -0500

    feat: added basic damage system
    adding basic damage/health system and health bar. currently just deletes player when health hits 0

--- Entities/engineer.gd
+++ Entities/engineer.gd
@@ -1,10 +1,23 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 150
-@export var max_health: int = 100
+@export var max_health: float = 100
 @export var is_enemy: bool = false
 
-@onready var current_health: int = max_health
+@onready var current_health: float = max_health
+@onready var health_bar = $HealthBar
+
+func _ready():
+	health_bar.init_health(max_health)
+
+func _take_damage(amount: float) -> void:
+	current_health -= amount
+	
+	health_bar.toggle_visible()
+	health_bar.health = current_health
+	
+	if current_health <= 0:
+		queue_free()
 
 func _physics_process(_delta):
 	var input_direction =  Input.get_vector("left", "right", "up", "down")
--- Entities/zombie.gd
+++ Entities/zombie.gd
@@ -3,13 +3,19 @@ extends CharacterBody2D
 @export var move_speed: float = 75
 @export var max_health: int = 100
 @export var is_enemy: bool = true
+@export var cooldown: = 1.0
 
 @onready var target = null
 @onready var current_health: int = max_health
 
+var can_attack = true
+
 func _enemy_check(entity):
 	return entity.get("is_enemy")
-	
+
+func _take_damage(amount: int) -> void:
+	current_health -= amount
+
 func _physics_process(delta):
 	if target != null:
 		var target_direction = global_position.direction_to(target.global_position)
@@ -28,3 +34,11 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		target = overlapping_bodies.filter(_enemy_check(body) == false).front()
 	else:
 		target = null
+
+
+func _on_attack_proximity_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body == target:
+		if can_attack:
+			body._take_damage(10)
+			can_attack = false;
+		get_tree().create_timer(cooldown).timeout.connect(func(): can_attack = true)
