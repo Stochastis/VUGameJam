1caa71117b396427e029d564f45e5a0aa633c017
typhus733 <typhus733@gmail.com>
2025-07-10 12:29:31 -0500

    feat: trimmed extra files

--- Assets/AssetDump/Roguelike BG/Map/Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Tiles" tilewidth="16" tileheight="16" margin="1" tilecount="1920" columns="60">
- <image source="../../../GODOT/ROGUEL/ASSETS/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
-</tileset>
