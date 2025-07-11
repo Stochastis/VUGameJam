ec7197c054aae5d260ba67a3fef1b164df10f267
Caleb Ross <stochastis3@gmail.com>
2025-07-11 16:01:45 -0500

    WIP: Componentizing Line of Sight

--- Components/line_of_sight.gd
+++ Components/line_of_sight.gd
@@ -1,35 +1,39 @@
-extends CharacterBody2D
+extends Node2D
+
+@export var ROTATIONSPEED: float = 8
+@export var parent: Node2D
+@export var observationArea: Area2D
+@export var trackedEntityName: String
 
 const MAXFLOAT: float = 10000000000000000
-var ROTATIONSPEED: float = 8
-var zombiePosition: Vector2
+var targetPosition: Vector2
+var observingEntity: bool = true
 
 # Line of sight debug
 var endPoint: Vector2 = Vector2(0, 0)
 var startPoint: Vector2 = Vector2(0, 0)
-var debugLines: bool = true
+@export var debugLines: bool = true
 
 func _draw() -> void:
 	if debugLines:
 		draw_line(startPoint, endPoint, Color.REBECCA_PURPLE, 2)
 
 func _ready() -> void:
-	zombiePosition = position + Vector2.from_angle(rotation)
+	targetPosition = parent.position + Vector2.from_angle(parent.rotation)
 
 func _process(delta: float) -> void:
-	var overlappingBodies: Array[Node2D] = $ObservationArea.get_overlapping_bodies()
-	# TODO: Change this method after merging in zombie changes so that it detects zombies instead of the enineer
-	var zombiesInRange: Array[Node2D] = overlappingBodies.filter(func(node): return node.name == "Engineer")
-	var observedZombies: Array[Node2D] = zombiesInRange.filter(func(node): return has_line_of_sight(node))
+	var overlappingBodies: Array[Node2D] = observationArea.get_overlapping_bodies()
+	var entitiesInRange: Array[Node2D] = overlappingBodies.filter(func(node): return node.name == trackedEntityName)
+	var observedEntities: Array[Node2D] = entitiesInRange.filter(func(node): return has_line_of_sight(node))
 	
-	if observedZombies.size() > 0:
-		zombiePosition = closestNode(observedZombies).position
-		$AnimatedSprite2D.set_frame_and_progress(1, 0)
+	if observedEntities.size() > 0:
+		observingEntity = true
+		targetPosition = closestNode(observedEntities).position
 	else:
-		$AnimatedSprite2D.set_frame_and_progress(0, 0)
+		observingEntity = false
 	
-	var to_zombie = (zombiePosition - position).normalized()
-	var desired_angle = to_zombie.angle()
+	var to_target = (targetPosition - position).normalized()
+	var desired_angle = to_target.angle()
 	rotation = lerp_angle(rotation, desired_angle, ROTATIONSPEED * delta)
 	
 	queue_redraw()
@@ -48,7 +52,7 @@ func closestNode(nodes: Array[Node2D]) -> Node2D:
 
 func has_line_of_sight(target: Node2D) -> bool:
 	startPoint = to_local(global_position)
-	endPoint = to_local(zombiePosition)
+	endPoint = to_local(targetPosition)
 	var query = PhysicsRayQueryParameters2D.create(global_position, target.global_position, 2, [self, target])
 	var collision := get_world_2d().direct_space_state.intersect_ray(query)
 	
