7419e063df21dd9f47f6ae03520716c72468c35c
Caleb Ross <stochastis3@gmail.com>
2025-07-11 13:53:09 -0500

    Update allied soldier animation on zombie detection
    Changed the allied soldier to update its animation frame when a zombie (currently 'Engineer') is detected in the observation area. The sprite switches to frame 1 when a zombie is present and reverts to frame 0 otherwise.

--- Entities/allied_soldier.gd
+++ Entities/allied_soldier.gd
@@ -10,9 +10,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var overlappingBodies: Array[Node2D] = $ObservationArea.get_overlapping_bodies()
 	# TODO: Change this method after merging in zombie changes so that it detects zombies instead of the enineer
-	var zombies: Array[Node2D] = overlappingBodies.filter(func(node): return node.name == "Engineer")
-	if zombies.size() > 0:
-		zombiePosition = closestNode(zombies).position
+	var observedZombies: Array[Node2D] = overlappingBodies.filter(func(node): return node.name == "Engineer")
+	
+	if observedZombies.size() > 0:
+		zombiePosition = closestNode(observedZombies).position
+		$AnimatedSprite2D.set_frame_and_progress(1, 0)
+	else:
+		$AnimatedSprite2D.set_frame_and_progress(0, 0)
+	
 	var to_zombie = (zombiePosition - position).normalized()
 	var desired_angle = to_zombie.angle()
 	rotation = lerp_angle(rotation, desired_angle, ROTATIONSPEED * delta)
