eb51927215eafeafcee6a2ab57db901d7991cfba
Caleb Ross <stochastis3@gmail.com>
2025-07-11 13:48:43 -0500

    Smooth allied soldier rotation towards target
    Updated allied_soldier.gd to smoothly rotate the soldier towards the detected target using lerp_angle instead of instantly snapping to the target's direction.

--- Entities/allied_soldier.gd
+++ Entities/allied_soldier.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 
 const MAXFLOAT: float = 10000000000000000
+var ROTATIONSPEED: float = 8
+var zombiePosition: Vector2
+
+func _ready() -> void:
+	zombiePosition = position
 
 func _process(delta: float) -> void:
 	var overlappingBodies: Array[Node2D] = $ObservationArea.get_overlapping_bodies()
 	# TODO: Change this method after merging in zombie changes so that it detects zombies instead of the enineer
 	var zombies: Array[Node2D] = overlappingBodies.filter(func(node): return node.name == "Engineer")
 	if zombies.size() > 0:
-		look_at(closestNode(zombies).position)
+		zombiePosition = closestNode(zombies).position
+	var to_zombie = (zombiePosition - position).normalized()
+	var desired_angle = to_zombie.angle()
+	rotation = lerp_angle(rotation, desired_angle, ROTATIONSPEED * delta)
+	print(zombiePosition)
 
 func closestNode(nodes: Array[Node2D]) -> Node2D:
 	var currClosestNode: Node2D = null
