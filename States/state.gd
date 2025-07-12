7c38140f83703b769aaa9b1645aca299ba366fdf
typhus733 <typhus733@gmail.com>
2025-07-12 13:25:51 -0500

    feat: adding state machines

---
+++ States/state.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name State
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+	
+func proccess_input(InputEvent) -> State:
+	return null
+
+func proccess_frame(InputEvent) -> State:
+	return null
+	
+func proccess_physics(InputEvent) -> State:
+	return null
