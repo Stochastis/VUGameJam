cb4d6228c26000521058f1288472080b09814ee0
Caleb Ross <stochastis3@gmail.com>
2025-07-18 16:34:00 -0500

    Squashed commit of the following:
    commit ff1e4adb79efd741f27a13ad1641315f17d03a0f
    Merge: 4142b53 085d00b
    Author: typhus733 <typhus733@gmail.com>
    Date:   Fri Jul 18 15:56:33 2025 -0500
    
        Merge branch 'main' into feat-zombie-basics
    
    commit 085d00b702d05ac91e17d3c987fb57eaae989ad1
    Author: Brandon Gunthner <66983826+typhus733@users.noreply.github.com>
    Date:   Fri Jul 18 15:49:19 2025 -0500
    
        feat: state machines foundation (#11)
    
    commit 4142b53f113d9eb066920dda6d3877239da7482c
    Author: typhus733 <typhus733@gmail.com>
    Date:   Sat Jul 12 11:08:46 2025 -0500
    
        feat: quick checkpoint
    
        moved health bar to components folder and added one to zombies, removed "Is_Enemy" in favor of an "Enemy" global group
    
    commit 1785decde5576c3e5e173968fdfc37541c0f99d9
    Merge: 24f4c68 576b464
    Author: typhus733 <typhus733@gmail.com>
    Date:   Fri Jul 11 16:31:53 2025 -0500
    
        Merge branch 'main' into feat-zombie-basics
    
    commit 24f4c68db8009cc85c39e4126391801c96316be9
    Author: typhus733 <typhus733@gmail.com>
    Date:   Thu Jul 10 12:20:53 2025 -0500
    
        feat: just starting on enemy navigation
    
    commit 33f423e57e13d960684df8487a35c227d7d414ef
    Author: typhus733 <typhus733@gmail.com>
    Date:   Mon Jul 7 23:54:24 2025 -0500
    
        feat: added basic damage system
    
        adding basic damage/health system and health bar. currently just deletes player when health hits 0
    
    commit 3ad6492430a30e1c39f39adfbdb214e39a6694fd
    Author: typhus733 <typhus733@gmail.com>
    Date:   Wed Jul 2 23:26:20 2025 -0500
    
        feat: zombies now focus based on proximity and ignore each other
    
    commit 0a6d2afa29cc7ef55c7cedd1680494cfe1711fed
    Author: typhus733 <typhus733@gmail.com>
    Date:   Wed Jul 2 21:09:09 2025 -0500
    
        feat: added zombie that chases player

---
+++ Entities/zombie.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 75
+@export var max_health: int = 100
+@export var cooldown: = 1.0
+
+@onready var target = null
+@onready var current_health: int = max_health
+
+var can_attack = true
+
+func _enemy_check(entity):
+	return entity.is_in_group("Enemies")
+
+func _take_damage(amount: int) -> void:
+	current_health -= amount
+
+func _physics_process(delta):
+	if target != null:
+		var target_direction = global_position.direction_to(target.global_position)
+		velocity = target_direction * move_speed
+		look_at(target.position)
+		move_and_slide()
+
+func _on_proximity_body_entered(body: Node2D) -> void:
+	if _enemy_check(body) == false:
+		target = body
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	var overlapping_bodies = %Chase_Proximity.get_overlapping_areas()
+	
+	if overlapping_bodies.any(func(): _enemy_check(body)):
+		target = overlapping_bodies.filter(_enemy_check(body) == false).front()
+	else:
+		target = null
+
+
+func _on_attack_proximity_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body == target:
+		if can_attack:
+			body._take_damage(10)
+			can_attack = false;
+		get_tree().create_timer(cooldown).timeout.connect(func(): can_attack = true)
