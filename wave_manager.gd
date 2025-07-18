c56c47c134e93d8f7f2b261e50f0431c1431ef35
Caleb Ross <stochastis3@gmail.com>
2025-07-18 15:57:56 -0500

    Basic spawning system
    Using templates and temporary objects/scenes just for testing

---
+++ wave_manager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var zombie: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_on_timer_timeout()
+
+func _on_timer_timeout() -> void:
+	spawn_zombie()
+	print("Spawned zombie")
+	#restart timer
+	$Timer.start(2)
+
+func spawn_zombie() -> void:
+	var zombieInstance = zombie.instantiate()
+	var spawnPoints = $SpawnPoints.get_children()
+	var spawnPoint: Vector2 = spawnPoints.pick_random().global_position
+	zombieInstance.global_position = spawnPoint
+	add_child(zombieInstance)
