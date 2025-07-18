7efd9995cdd13b118eae0c54fc1e63ea898f6ec5
typhus733 <typhus733@gmail.com>
2025-07-18 15:48:12 -0500

    feat: finished basic implementation!
    Fixed the bad method name and removed the idle state from allied soldier now that I've seen it work

--- States/npc_idle.gd
+++ States/npc_idle.gd
@@ -6,20 +6,20 @@ class_name npc_idle
 var move_direction: Vector2
 var wander_cycle: float
 
-func randomize():
+func randomize_values():
 	move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	wander_cycle = randf_range(1, 3)
+	wander_cycle = randf_range(0, 1.5)
 
 func enter():
-	randomize()
+	randomize_values()
 
 func update(delta: float):
 	if wander_cycle > 0:
 		wander_cycle -= delta
 	else:
-		randomize()
+		randomize_values()
 
 func physics_update(delta: float):
 	if entity:
-		entity.velocity = move_direction * entity.move_speed
+		entity.velocity = move_direction * (entity.move_speed * 0.25)
 		entity.move_and_slide()
