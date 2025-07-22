5bae369ba992d5ca4207c06101693e3c929d1b34
Caleb Ross <stochastis3@gmail.com>
2025-07-22 13:20:33 -0500

    Minor LineOfSight Refactor
    Updated the line of sight component so that multiple collision layers can be detected. Made it so allied soldiers can look over the "outer walls" while zombies cannot.

--- Components/observer.gd
+++ Components/observer.gd
@@ -4,13 +4,9 @@ class_name Observer
 @export var lineOfSight: LineOfSight
 @export var observationArea: Area2D
 
-const WALLMASK: int = 2
-
 var observedEntities: Array[Node2D]
 
 func _process(_delta: float) -> void:
 	if observationArea and lineOfSight:
 		var bodiesInArea: Array[Node2D] = observationArea.get_overlapping_bodies()
-		observedEntities = bodiesInArea.filter(func(node): return lineOfSight.has_line_of_sight(node, WALLMASK))
-		if lineOfSight.parent.name == "Zombie":
-			print(observationArea.name)
+		observedEntities = bodiesInArea.filter(func(node): return lineOfSight.has_line_of_sight(node))
