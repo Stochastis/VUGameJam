68731a28bc2512d7cf7546dc7f8c32e4de60fd5b
Caleb Ross <stochastis3@gmail.com>
2025-07-22 10:47:58 -0500

    Add entity tracking to observer
    Introduces trackedEntityGroups and trackedEntityNames to observer.gd for flexible entity observation based on group or name, with line of sight filtering. Removes unused trackedEntityName export from allied_soldier.gd.

--- Components/observer.gd
+++ Components/observer.gd
@@ -1,1 +1,19 @@
 extends Node2D
+
+@export var trackedEntityGroups: Array[String]
+@export var trackedEntityNames: Array[String]
+@export var lineOfSight: LineOfSight
+
+const WALLMASK: int = 2
+
+var observedEntities: Array[Node2D]
+
+func _process(_delta: float) -> void:
+	var bodiesInArea: Array[Node2D] = $ObservationArea.get_overlapping_bodies()
+	var relevantBodiesInArea: Array[Node2D]
+	for group in trackedEntityGroups:
+		relevantBodiesInArea.append_array(bodiesInArea.filter(func(node): return node.is_in_group(group) and not relevantBodiesInArea.has(node)))
+	for name in trackedEntityNames:
+		relevantBodiesInArea.append_array(bodiesInArea.filter(func(node): return node.name == name and not relevantBodiesInArea.has(node)))
+	
+	observedEntities = relevantBodiesInArea.filter(func(node): return lineOfSight.has_line_of_sight(node, WALLMASK))
--- Entities/allied_soldier.gd
+++ Entities/allied_soldier.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var move_speed: float = 100
 @export var lineOfSight: LineOfSight
-@export var trackedEntityName: String
 @export var ROTATIONSPEED: float = 8
 @export var debugLines: bool = true
 
