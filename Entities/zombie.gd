d4d37df1731e465171c14f393c8918feb7d2aaeb
Stochastis <stochastis3@gmail.com>
2025-07-26 13:59:01 -0500

    Squashed commit of the following:
    commit 6175f19629c3acf73f22fecf60e349bf520cacd3
    Author: Brandon Gunthner <66983826+typhus733@users.noreply.github.com>
    Date:   Sat Jul 26 11:30:44 2025 -0500
    
        fix: zombies damage player (#24)
    
        * fix: attempting to fix zombie damaging player
    
        * feat: trying to mirror allied soldier shoot with zombie attack
    
        * feat: call attack on update and remove parent target set
    
        * feat: damage triggers, need to fix health bar
    
        * feat: zombie flashes on attack, still need to fix player health bar
    
        * feat: health bar updates properly and hides until first update

--- Entities/zombie.gd
+++ Entities/zombie.gd
@@ -6,15 +6,6 @@ extends CharacterBody2D
 
 var can_attack = true
 
-func _on_attack_proximity_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
-	if body == target:
-		if can_attack and body.has_node("HealthSystem"):
-			body.get_node("HealthSystem").damage(10)
-			can_attack = false;
-		elif not body.has_node("HealthSystem"):
-			printerr("body does not have HealthSystem component")
-		get_tree().create_timer(cooldown).timeout.connect(func(): can_attack = true)
-
 func _on_health_system_health_changed() -> void:
 	if $HealthSystem.currHealth <= 0:
 		queue_free()
--- States/npc_targeting_zombie.gd
+++ States/npc_targeting_zombie.gd
@@ -1,21 +1,44 @@
 extends NpcTargeting
 class_name npc_targeting_zombie
 
+
 @export var navAgent: NavigationAgent2D
 @export var parent: CharacterBody2D
 @export var npcTargetingZombieMoveSpeed: float = 20
 @export var chaseObservationArea: Area2D
+@export var attackArea: Area2D
 @export var observer: Observer
+@export var attack_cooldown: float
 
+var nextPathPos: Vector2
+
+func attack() -> void:
+	if !targeter.targetNode:
+		return
+	attackArea = parent.get_node("Attack_Proximity")
+	if targeter.targetNode.has_node("HealthSystem") && attackArea.overlaps_body(targeter.targetNode):
+		var targetHealthSystem: HealthSystem = targeter.targetNode.get_node("HealthSystem")
+		targetHealthSystem.damage(10)
+		var sprite : Sprite2D = parent.get_node("Pivot_Point/Sprite")
+		sprite.modulate = Color.DARK_GRAY
+		await get_tree().create_timer(0.1).timeout
+		sprite.modulate = Color.WHITE
+		
 func physics_update(_delta: float) -> void:
 	var nextPathPos: Vector2 = navAgent.get_next_path_position()
 	var toNextPath = (nextPathPos - parent.global_position).normalized()
 	parent.velocity = toNextPath * npcTargetingZombieMoveSpeed
 	parent.move_and_slide()
 
+func _on_attack_timer_timeout() -> void:
+	attack()
+	$AttackTimer.start(attack_cooldown)
+
 func reNav() -> void:
 	#Re-nav once per second since the target can move
 	navAgent.target_position = targeter.targetPosition
+	nextPathPos = navAgent.get_next_path_position()
+	$AttackTimer.start(attack_cooldown)
 
 func enter() -> void:
 	reNav()
