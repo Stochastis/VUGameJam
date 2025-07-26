6175f19629c3acf73f22fecf60e349bf520cacd3
Brandon Gunthner <66983826+typhus733@users.noreply.github.com>
2025-07-26 11:30:44 -0500

    fix: zombies damage player (#24)
    * fix: attempting to fix zombie damaging player
    
    * feat: trying to mirror allied soldier shoot with zombie attack
    
    * feat: call attack on update and remove parent target set
    
    * feat: damage triggers, need to fix health bar
    
    * feat: zombie flashes on attack, still need to fix player health bar
    
    * feat: health bar updates properly and hides until first update

--- Components/health_bar.gd
+++ Components/health_bar.gd
@@ -1,9 +1,15 @@
 extends ProgressBar
 
-@export var healthSystem: Node
+@export var healthSystem: HealthSystem
 
 func _ready() -> void:
 	max_value = healthSystem.maxHealth
+	visible = false
+	if not healthSystem.healthChanged.is_connected(_on_health_system_health_changed):
+		var error = healthSystem.healthChanged.connect(_on_health_system_health_changed)
+		if error != OK:
+			push_error("Failed to connect healthChanged signal")
 
 func _on_health_system_health_changed() -> void:
+	visible = true
 	value = healthSystem.currHealth
--- States/npc_targeting_zombie.gd
+++ States/npc_targeting_zombie.gd
@@ -1,24 +1,44 @@
 extends NpcTargeting
 class_name npc_targeting_zombie
 
+
 @export var navAgent: NavigationAgent2D
 @export var parent: CharacterBody2D
 @export var moveSpeed: float = 20
 @export var idleObservationArea: Area2D
 @export var chaseObservationArea: Area2D
+@export var attackArea: Area2D
 @export var observer: Observer
+@export var attack_cooldown: float
 
 var nextPathPos: Vector2
 
+func attack() -> void:
+	if !targeter.targetNode:
+		return
+	attackArea = parent.get_node("Attack_Proximity")
+	if targeter.targetNode.has_node("HealthSystem") && attackArea.overlaps_body(targeter.targetNode):
+		var targetHealthSystem: HealthSystem = targeter.targetNode.get_node("HealthSystem")
+		targetHealthSystem.damage(10)
+		var sprite : Sprite2D = parent.get_node("Pivot_Point/Sprite")
+		sprite.modulate = Color.DARK_GRAY
+		await get_tree().create_timer(0.1).timeout
+		sprite.modulate = Color.WHITE
+		
 func physics_update(_delta: float) -> void:
 	var toNextPath = (nextPathPos - parent.global_position).normalized()
 	parent.velocity = toNextPath * moveSpeed
 	parent.move_and_slide()
 
+func _on_attack_timer_timeout() -> void:
+	attack()
+	$AttackTimer.start(attack_cooldown)
+
 func _on_nav_timer_timeout() -> void:
 	#Re-nav since the target can move
 	navAgent.target_position = targeter.targetPosition
 	nextPathPos = navAgent.get_next_path_position()
+	$AttackTimer.start(attack_cooldown)
 
 func enter() -> void:
 	observer.observationArea = chaseObservationArea
