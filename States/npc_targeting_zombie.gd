4b5f5b64387ea154155463b389e6b669f4737291
Stochastis <stochastis3@gmail.com>
2025-07-26 14:24:05 -0500

    Outer walls now destructible

--- States/npc_targeting_zombie.gd
+++ States/npc_targeting_zombie.gd
@@ -1,6 +1,5 @@
 extends NpcTargeting
-class_name npc_targeting_zombie
-
+class_name NpcTargetingZombie
 
 @export var navAgent: NavigationAgent2D
 @export var parent: CharacterBody2D
@@ -15,17 +14,17 @@ var nextPathPos: Vector2
 func attack() -> void:
 	if !targeter.targetNode:
 		return
-	attackArea = parent.get_node("Attack_Proximity")
 	if targeter.targetNode.has_node("HealthSystem") && attackArea.overlaps_body(targeter.targetNode):
+		print("health system acquired and overlapping for attack")
 		var targetHealthSystem: HealthSystem = targeter.targetNode.get_node("HealthSystem")
 		targetHealthSystem.damage(10)
 		var sprite : Sprite2D = parent.get_node("Pivot_Point/Sprite")
 		sprite.modulate = Color.DARK_GRAY
 		await get_tree().create_timer(0.1).timeout
 		sprite.modulate = Color.WHITE
-		
+
 func physics_update(_delta: float) -> void:
-	var nextPathPos: Vector2 = navAgent.get_next_path_position()
+	nextPathPos = navAgent.get_next_path_position()
 	var toNextPath = (nextPathPos - parent.global_position).normalized()
 	parent.velocity = toNextPath * npcTargetingZombieMoveSpeed
 	parent.move_and_slide()
@@ -37,13 +36,13 @@ func _on_attack_timer_timeout() -> void:
 func reNav() -> void:
 	#Re-nav once per second since the target can move
 	navAgent.target_position = targeter.targetPosition
-	nextPathPos = navAgent.get_next_path_position()
-	$AttackTimer.start(attack_cooldown)
 
 func enter() -> void:
 	reNav()
 	$NavTimer.start()
+	$AttackTimer.start(attack_cooldown)
 	observer.observationArea = chaseObservationArea
 
 func exit() -> void:
 	$NavTimer.stop()
+	$AttackTimer.stop()
