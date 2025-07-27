70573e557822c8dee402eb1d1d8994c5689b6da7
Stochastis <stochastis3@gmail.com>
2025-07-27 14:25:06 -0500

    Moved repair/replace to own functions

--- Entities/engineer.gd
+++ Entities/engineer.gd
@@ -8,31 +8,26 @@ const MAXFLOAT: float = 10000000000000000
 var repairing: bool = false
 var replacing: bool = false
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
-		interact()
-	if event.is_action_pressed("repair"):
-		repairing = true
-	if event.is_action_released("repair"):
-		repairing = false
-	if event.is_action_pressed("replace"):
-		replacing = true
-	if event.is_action_released("replace"):
-		replacing = false
-
 func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
-	
-	#Can't do both at the same time. Prioritize repairing over replacing.
-	if repairing:
-		print("repairing")
-	elif replacing:
-		print("replacing")
 
 func _physics_process(_delta):
 	var input_direction =  Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * move_speed
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		interact()
+		
+	if event.is_action_pressed("repair"):
+		beginRepairing()
+	if event.is_action_released("repair"):
+		stopRepairing()
+	if event.is_action_pressed("replace"):
+		beginReplacing()
+	if event.is_action_released("replace"):
+		stopReplacing()
 
 func interact() -> void:
 	var overlappingAreas = playerInteractionArea.get_overlapping_areas()
@@ -60,3 +55,18 @@ func closestInteractableArea(areas: Array[Area2D]) -> Area2D:
 			distanceToClosest = distanceToArea
 	
 	return closestOverlappingArea
+
+#Can't do both at the same time. Prioritize repairing over replacing.
+func beginRepairing() -> void:
+	repairing = true
+	if replacing:
+		stopReplacing()
+func stopRepairing() -> void:
+	repairing = false
+	if Input.is_action_pressed("replace"):
+		beginReplacing()
+func beginReplacing() -> void:
+	if not repairing:
+		replacing = true
+func stopReplacing() -> void:
+	replacing = false
