8344fec52b4ba8bac82f14e881fa3d708636fdd5
Stochastis <stochastis3@gmail.com>
2025-07-27 20:32:01 -0500

    Health bar simplification

--- Components/health_bar.gd
+++ Components/health_bar.gd
@@ -10,6 +10,10 @@ func _ready() -> void:
 		if error != OK:
 			push_error("Failed to connect healthChanged signal")
 
+func _process(delta: float) -> void:
+	self.rotation = -get_parent().rotation
+	self.global_position = get_parent().global_position + Vector2(-8, -8)
+
 func _on_health_system_health_changed() -> void:
 	visible = true
 	value = healthSystem.currHealth
