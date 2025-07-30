c61a0ebdb30fddafc443fa952ad2850c8ff255fa
Stochastis <stochastis3@gmail.com>
2025-07-29 23:48:48 -0500

    Scoring system

--- project/game_over.gd
+++ project/game_over.gd
@@ -1,7 +1,12 @@
 extends Node
 
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var score_amount: Label = $Control/GameOver/YourFinalScore/ScoreAmount
 
 func _ready() -> void:
+	ScoreKeeper.finalScore = ScoreKeeper.score
+	score_amount.text = str(ScoreKeeper.finalScore)
+	ScoreKeeper.score = 0
+	
 	audio_stream_player.pitch_scale = randf_range(0.9, 1.1)
 	audio_stream_player.play()
